38cecd293c73fc420fbfa1bf304cb6c309e6ed77
YKTECHOKILLL <ardyadiyol@gmail.com>
2025-02-19 13:12:01 +0700

    Create ardya.py

---
+++ Team/ardya.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Ardya Zahira Ardhana"
+    nim = "22.130.10.371"
+    peran = "Menjabat Sebagai Presenter"
+    perkenalan(nama, nim, peran)
