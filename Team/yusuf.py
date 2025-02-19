653136c49f89f65f1b3ad9c2a588ee96ecbbd6d5
YKTECHOKILLL <yusufkurniawanaee@gmail.com>
2025-02-19 13:05:11 +0700

    Create yusuf.py

---
+++ Team/yusuf.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Yusuf Kurniawan"
+    nim = "22.130.10.350"
+    peran = "Menjabat Sebagai Desainer"
+    perkenalan(nama, nim, peran)
