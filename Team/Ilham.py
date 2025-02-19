2c65465a6fcdba1f058c241c8f306a2ce45cadfa
IlharshFerocity <161426872+IlharshFerocity@users.noreply.github.com>
2025-02-19 14:30:13 +0700

    Create Ilham.py

---
+++ Team/Ilham.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Ilham Pebrianto Eko Saputra"
+    nim = "22.130.10.378"
+    peran = "Menjabat Sebagai Programmer"
+    perkenalan(nama, nim, peran)
