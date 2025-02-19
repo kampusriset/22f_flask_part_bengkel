b533fe6cb315c684dfb760a3836569c5698352dd
cilokkuah <muhramaki@gmail.com>
2025-02-19 12:56:52 +0700

    Create rafimaliki.py

---
+++ Team/rafimaliki.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "RAfi MAliki"
+    nim = "22.130.10.348"
+    peran = "Leader"
+    perkenalan(nama, nim, peran)
